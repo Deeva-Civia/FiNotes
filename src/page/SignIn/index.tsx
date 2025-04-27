e0655d5654f31f419f85efb4ab4c81153a349407
yesa2711 <yesamokolensang@gmail.com>
2025-04-27 14:15:52 +0800

    add TextInput in Signin

--- src/page/SignIn/index.tsx
+++ src/page/SignIn/index.tsx
@@ -4,7 +4,20 @@ import {Header, TextInput} from '../../components/molecules';
 import {Button} from '../../components/atoms/';
 
 const SignIn = () => {
-  return <View />;
+  return (
+    <View style={styles.pageContainer}>
+      <Header title="Sign In" />
+      <View style={styles.contentContainer}>
+        <TextInput
+          label="Username / Email Address"
+          placeholder="Enter your Username"
+        />
+        <TextInput label="Password" placeholder="Enter your Password" />
+        <Button label="Sign In" />
+        <Button label="Creat New Account" />
+      </View>
+    </View>
+  );
 };
 
 export default SignIn;
